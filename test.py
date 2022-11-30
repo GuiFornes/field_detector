55eb43dc85fd5ea6944e622768bf07df2a72e21d
GuiFornes <fornesguillaume@gmail.com>
2022-11-30 15:10:03 +0100

    diagram uml + connectedComponnents working

--- test.py
+++ test.py
@@ -5,17 +5,17 @@ from matplotlib import pyplot as plt
 KERNEL = np.ones((5, 5), np.uint8)
 LOWER_BOUND, UPPER_BOUND = (28, 30, 40), (67, 228, 144)
 
+
 def color_filter(image_bgr, lower_bound, upper_bound):
     hsv = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2HSV)
-    mask_ = cv2.inRange(hsv, lower_bound, upper_bound)
-    return cv2.bitwise_and(image_bgr, image_bgr, mask=mask_)
+    return cv2.inRange(hsv, lower_bound, upper_bound)
 
 
 def equalize_hist(image_bgr):
     """ Equalize histogram of the image, does it work ? """
-    image_yuv = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2YUV)
-    image_yuv[:, :, 0] = cv2.equalizeHist(image_yuv[:, :, 0])
-    return cv2.cvtColor(image_yuv, cv2.COLOR_YUV2BGR)
+    image_hsv = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2HSV)
+    image_hsv[:, :, 0] = cv2.equalizeHist(image_hsv[:, :, 0])
+    return cv2.cvtColor(image_hsv, cv2.COLOR_YUV2BGR)
 
 
 def show_histogram(image_bgr):
@@ -25,33 +25,30 @@ def show_histogram(image_bgr):
 
 
 def mask_generator(image_bgr):
-    green = color_filter(image_bgr,  LOWER_BOUND, UPPER_BOUND)
-    thresh = np.where(green > 0, 255, 0).astype(np.uint8)
-    tmp = cv2.dilate(thresh, KERNEL, iterations=1)
+    mask = color_filter(image_bgr, LOWER_BOUND, UPPER_BOUND)
+    tmp = cv2.dilate(mask, KERNEL, iterations=1)
     tmp = cv2.erode(tmp, KERNEL, iterations=1)
     tmp = cv2.erode(tmp, KERNEL, iterations=1)
     green_mask = cv2.dilate(tmp, KERNEL, iterations=1)
-    return green_mask, thresh, green
+    return green_mask
 
 
-def isolate_largest_contour(mask):
+def isolate_field(mask):
     """ Isolate the largest contour in the mask """
     # Need to change the mask format !
-    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    if len(contours) == 0:
-        return None
-    return cv2.drawContours(mask, [max(contours, key=cv2.contourArea)], -1, (0, 255, 0), -1)
+    _, labels = cv2.connectedComponents(mask)
+    largest_label = np.argmax(np.bincount(labels.flat)[1:]) + 1
+    return np.where(labels == largest_label, 255, 0).astype(np.uint8)
+
 
 img = cv2.imread('./data/log1/001-rgb.png')
 img = cv2.GaussianBlur(img, (3, 3), 0)
-mask, thr, img_green = mask_generator(img)
-# final_mask = isolate_largest_contour(mask)
+green_mask = mask_generator(img)
+field_mask = isolate_field(green_mask)
 
 cv2.imshow('frame', img)
-cv2.imshow('green', img_green)
-cv2.imshow('thresh', thr)
-cv2.imshow('mask', mask)
-# cv2.imshow('final_mask', final_mask)
+cv2.imshow('mask', green_mask)
+cv2.imshow('green', field_mask)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
